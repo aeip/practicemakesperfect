083e26dff25f22820f6e008dd8a473c1879aa29e
Stasys Meclazcke <aeip@live.com>
2014-03-08 23:59:22 -0700

    Some changes to hobbies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309063300) do
+ActiveRecord::Schema.define(version: 20140309065713) do
 
   create_table "hobbies", force: true do |t|
     t.string   "description"
